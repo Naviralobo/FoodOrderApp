49fab849d89e29020c5e0d5fc99ffcc95165ac41
Naviralobo <majel.josephine>
2022-09-17 15:08:28 +0530

    Making Items Visible

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-import MealsSummary from "../Meals/MealsSummary";
 
 import mealsImage from "../../assets/meals.jpg";
 
@@ -16,7 +15,6 @@ const Header = (props) => {
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Table fulll of delicious foods" />
       </div>
-      <MealsSummary />
     </Fragment>
   );
 };
